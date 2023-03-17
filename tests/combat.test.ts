a0cc9de51aeb6d792ba4eccbbd42f3599543cb83
j0bster <stouthartjob@gmail.com>
2023-03-17 17:32:15 +0100

    Added test to check if combat system cycles properly.

--- tests/combat.test.ts
+++ tests/combat.test.ts
@@ -26,4 +26,51 @@ describe ('Combat', () => {
         CombatIntro(combat);
         expect(combat.combatState.priorityByEntity).toEqual({1: 10, 2: 5});
     })
+    test('getNextEntity', () => {
+        const world = createWorld();
+        const entity = 1;
+        const component = {
+            dexterity: 10,
+            health: 10
+        };
+        createEntity(world, entity, component, 'STATS');
+        const entity2 = 2;
+        const component2 = {
+            dexterity: 5,
+            health: 10
+        };
+        createEntity(world, entity2, component2, 'STATS');
+        const combat = {
+            combatants: world.entities,
+            combatState: {
+                priorityByEntity: {}
+            }
+        };
+        CombatIntro(combat);
+        expect(getNextEntity(combat)).toBe(1);
+    })
+    test('doTurn', () => {
+        const world = createWorld();
+        const entity = 1;
+        const component = {
+            dexterity: 10,
+            health: 10
+        };
+        createEntity(world, entity, component, 'STATS');
+        const entity2 = 2;
+        const component2 = {
+            dexterity: 5,
+            health: 10
+        };
+        createEntity(world, entity2, component2, 'STATS');
+        const combat = {
+            combatants: world.entities,
+            combatState: {
+                priorityByEntity: {}
+            }
+        };
+        CombatIntro(combat);
+        doTurn(1, combat);
+        expect(combat.combatState.priorityByEntity).toEqual({1: 9, 2: 5});
+    })
 })
