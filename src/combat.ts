cc053c3aeb95d0f50b4d400cf90ac8a4bf36937d
j0bster <stouthartjob@gmail.com>
2023-03-17 17:11:16 +0100

    Changed combat system.

--- src/combat.ts
+++ src/combat.ts
@@ -1,97 +1,41 @@
-import { Player } from './player';
-import { Enemy } from './enemy';
+import { createWorld, createEntity, addComponent } from './enitity';
 
-
-class Combat {
-    player: Player;
-    enemy: Enemy;
-    order: any[] = [];
-
-    constructor(player: Player, enemy: Enemy) {
-        this.player = player;
-        this.enemy = enemy;
-        this.order = [];
-    }
-
-    public start() {
-        console.log('Combat started! You are fighting a ' + this.enemy.name + '!');
-        this.Turn_order();
-        console.log(this.order);
-
-        while(true) {
-            let attacker: Player | Enemy = this.order.shift();
-            this.order.push(attacker);
-            let target = this.order.filter((x) => x != attacker)[0];
-            // console.log(target);
-            this.Atack(attacker, target);
-            if (this.player.data.hp <= 0) {
-                console.log('You died!');
-                break;
-            }
-            else if (this.enemy.data.hp <= 0) {
-                console.log('You won!');
-                break;
-            }
-        }
-
-    }
-
-    private Turn_order() {
-        let mult = 1.5;
-        if (this.player.data.spd > this.enemy.data.spd) {
-            this.order.push(this.player);
-            while (this.enemy.data.spd * mult < this.player.data.spd) {
-                this.order.push(this.player);
-                mult += 1.5;
-            }
-            this.order.push(this.enemy);
-        }
-        else {
-            this.order.push(this.enemy);
-            while (this.player.data.spd * mult < this.enemy.data.spd) {
-                this.order.push(this.enemy);
-                mult += 1.5;
-            }
-            this.order.push(this.player);
-        }
-    }
-
-    private Atack(attacker: Player | Enemy, target: Player | Enemy) {
-        let damage = attacker.data.atk - target.data.def;
-        if (damage < 0) {
-            damage = 0;
-        }
-        target.data.hp -= damage;
-        console.log(attacker.name + ' attacked ' + target.name + ' for ' + damage + ' damage!');
-    }
-
-}
-
-let player: Player = {
-    id: 1,
-    name: 'test',
-    data: {
-        hp: 100,
-        mp: 150,
-        atk: 100,
-        def: 100,
-        spd: 101,
-    },
-    skills: [],
+type CombatState = {
+    priorityByEntity: Record<number, number>;
 };
 
-let enemy: Enemy = {
-    id: 1,
-    name: 'test-Enemy',
-    data: {
-        hp: 100,
-        mp: 100,
-        atk: 99,
-        def: 99,
-        spd: 100,
-    },
-    skills: [],
-};  
+type Combat = {
+    combatants: Map<number, Map<string, any>>;
+    combatState: CombatState;
+};
 
-let combat = new Combat(player, enemy);
-combat.start();
+export function CombatIntro(combat: Combat) {
+    const { priorityByEntity } = combat.combatState;
+    combat.combatants.forEach((value, key) => {
+        priorityByEntity[key] = combat.combatants.get(key)?.get('STATS')?.dexterity;
+    });
+}
+
+
+export function getNextEntity(combat: Combat) {
+    const { priorityByEntity } = combat.combatState;
+    const entities = Array.from(combat.combatants.keys());
+    const entity = entities.reduce((acc, cur) => {
+        if (priorityByEntity[cur] > priorityByEntity[acc]) {
+            return cur;
+        }
+        return acc;
+    }, entities[0]);
+    return entity;
+}
+
+export function doTurn(entity: number, combat: Combat) {
+    const { priorityByEntity } = combat.combatState;
+    priorityByEntity[entity] -= 1;
+
+    // reset priority if it reaches 0
+    if (priorityByEntity[entity] === 0) {
+        priorityByEntity[entity] = combat.combatants.get(entity)?.get('STATS')?.dexterity;
+    }
+}
+
